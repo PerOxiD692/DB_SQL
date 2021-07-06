83e81e1ca38cbfabc7106c38b973d9a63958c63b
PerOxiD692 <jan-daniel.martin@gmx.de>
2021-07-06 09:11:56 +0000

    NOT NULL + DEFAULT;  Gleiche Einträge werden zugelassen

---
+++ 02_SQL-Tables/02b_db_table_sql.sql
@@ -0,0 +1,44 @@
+/* ----- Strukturen ----- */
+
+/* ----- Datenbank test_db löschen, falls vorhanden ----- */
+DROP DATABASE IF EXISTS test_db;
+
+
+/* ----- Datenbanken anlegen, falls noch nicht vorhanden ----- */
+CREATE DATABASE IF NOT EXISTS test_db;
+
+
+/* ----- Datenbank auswählen ----- */
+USE test_db;
+
+/* ----- Tabelle löschen, falls vorhanden ----- */
+DROP TABLE IF EXISTS test_db.test_table;
+
+/* ----- Tabelle anlegen, falls nicht vorhanden ----- */
+CREATE TABLE IF NOT EXISTS test_table
+(
+    name VARCHAR(20) NOT NULL DEFAULT "To be announced",
+    age INT NOT NULL DEFAULT 0
+);
+
+
+/* ----- Struktur der Tabelle anzeigen ------ */
+DESC test_table;
+
+
+/* ----- Daten einfügen ----- */
+INSERT INTO test_table(name,age) VALUES ("Grizabella",29);
+INSERT INTO test_table(name,age) VALUES ("Alonzo",35);
+INSERT INTO test_table() VALUES();
+
+# Doppelte Datensätze werden zugelassen
+INSERT INTO test_table(name,age) VALUES ("Alonzo",35);
+
+/* ----- Tabelleninhalt anzeigen ----- */
+SELECT * FROM test_table;
+
+
+
+
+
+
