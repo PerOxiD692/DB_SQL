14363743ba7a9d7937efc6b9963313662b0b3b00
PerOxiD692 <jan-daniel.martin@gmx.de>
2021-07-07 12:55:31 +0000

    Import aus Tabelle ohne ID | Erstellen einer ID Spalte mit AUTO_INKREMENT

---
+++ 04_SQL_data_import/04a_db_csv_import_SQL.sql
@@ -0,0 +1,26 @@
+/* ----- Datenbank erzeugen ----- */
+DROP DATABASE IF EXISTS user_db; #Lรถsche Datenbank "user_db" falls vorhanden
+CREATE DATABASE IF NOT EXISTS user_db; #Erzeuge Datenbank "user_db" falls nicht vorhanden
+USE user_db; #Nutze die Datenbank "user_db"
+
+/* ----- Tabelle Cats ----- */
+DROP TABLE IF EXISTS cats; #Lรถsche Tabelle "cats" falls vorhanden
+CREATE TABLE IF NOT EXISTS cats #Erzeuge Tabelle "cats" falls nicht vorhanden
+(
+    id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+    cat_name VARCHAR(20) NOT NULL,
+    fur_color VARCHAR(20) NOT NULL,
+    age INT NOT NULL
+);
+
+/* ----- Struktur der Tabelle anzeigen ----- */
+DESCRIBE cats; #Zeigt die Struktur der Tabelle "cats"
+
+/* CSV-Daten parsen */
+LOAD DATA LOCAL INFILE "04_SQL_data_import/cats_export_no_id.csv" INTO TABLE cats #Lade Daten in Tabelle Cats
+FIELDS TERMINATED BY "," LINES TERMINATED BY "\n" #Felder werden getrennt durch ";" | Zeilen werden gertrennt durch den Umbruch "\n"
+IGNORE 1 ROWS (cat_name, fur_color, age); #Die erste Zeile wird ignoriert
+
+
+/* ----- Gesamte Tabelle ausgeben ----- */
+SELECT * FROM cats; #gibt Tabelle "cats" aus
