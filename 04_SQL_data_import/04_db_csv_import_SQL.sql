bb0ed808290d17c3d79a843b76e36a7b78b79015
PerOxiD692 <jan-daniel.martin@gmx.de>
2021-07-07 10:49:10 +0000

    Import der Daten in Tabelle cats aus cats_export.csv LOAD DATA LOCAL INFILE | ITO TABLE | FIELDS TERMINATED BY | LINES TERMINATED BY | IGNORE ROWS

--- 04_SQL_data_import/04_db_csv_import_SQL.sql
+++ 04_SQL_data_import/04_db_csv_import_SQL.sql
@@ -1,11 +1,11 @@
 /* ----- Datenbank erzeugen ----- */
-DROP DATABASE IF EXISTS user_db;
-CREATE DATABASE IF NOT EXISTS user_db;
-USE user_db;
+DROP DATABASE IF EXISTS user_db; #Lösche Datenbank "user_db" falls vorhanden
+CREATE DATABASE IF NOT EXISTS user_db; #Erzeuge Datenbank "user_db" falls nicht vorhanden
+USE user_db; #Nutze die Datenbank "user_db"
 
 /* ----- Tabelle Cats ----- */
-DROP TABLE IF EXISTS cats;
-CREATE TABLE IF NOT EXISTS cats
+DROP TABLE IF EXISTS cats; #Lösche Tabelle "cats" falls vorhanden
+CREATE TABLE IF NOT EXISTS cats #Erzeuge Tabelle "cats" falls nicht vorhanden
 (
     id INT NOT NULL PRIMARY KEY,
     cat_name VARCHAR(20) NOT NULL,
@@ -14,4 +14,13 @@ CREATE TABLE IF NOT EXISTS cats
 );
 
 /* ----- Struktur der Tabelle anzeigen ----- */
-DESCRIBE cats;
+DESCRIBE cats; #Zeigt die Struktur der Tabelle "cats"
+
+/* CSV-Daten parsen */
+LOAD DATA LOCAL INFILE "04_SQL_data_import/cats_export.csv" INTO TABLE cats #Lade Daten in Tabelle Cats
+FIELDS TERMINATED BY ";" LINES TERMINATED BY "\n" #Felder werden getrennt durch ";" | Zeilen werden gertrennt durch den Umbruch "\n"
+IGNORE 1 ROWS; #Die erste Zeile wird ignoriert
+
+
+/* ----- Gesamte Tabelle ausgeben ----- */
+SELECT * FROM cats; #gibt Tabelle "cats" aus
