9d09b2377f523831ddb0b57b1618e3efcb2a5adf
PerOxiD692 <jan-daniel.martin@gmx.de>
2021-07-09 06:59:53 +0000

    05_4 - WHERE (AND | OR | NOT) Filter

--- 05_SQL_SELECT/05_4_que_filter.sql
+++ 05_SQL_SELECT/05_4_que_filter.sql
@@ -21,6 +21,11 @@ SELECT
     dividend AS "Dividende"
 FROM ccc_list
 # Einzeldaten / Stings
-WHERE sector = "Communication Services"
+# WHERE sector = "Communication Services"
+# WHERE industry = "Media"
+# WHERE sector = "Communication Services" AND industry = "Entertainment" # Kombination mit AND
+# WHERE industry = "Entertainment" AND payouts = 4
+# WHERE sector = "Communication Services" AND (industry = "Entertainment" OR industry = "Media") # Kombination durch AND / OR
+WHERE sector = "Communication Services" AND NOT industry = "Media"
 ORDER BY price ASC    #beginnend mit der günstigster Aktie
 LIMIT 20;
